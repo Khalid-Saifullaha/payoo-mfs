221307a118b2579876943b5172687291826421a4
Your Name <khalidsaifullaha3@gmail.com>
2024-09-18 23:21:53 +0600

    added simple login handler and prevent default

---
+++ js/login.js
@@ -0,0 +1,9 @@
+
+// search: from submit reloading the pages
+
+document.getElementById('button-login').addEventListener('click', function(event){
+    event.preventDefault();
+    const phoneNumber = document.getElementById('phone-number').value;
+    console.log(phoneNumber);
+
+})
