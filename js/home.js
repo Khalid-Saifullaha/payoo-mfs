b42af4cb1711140a40e86c9de8b990405847553b
Your Name <khalidsaifullaha3@gmail.com>
2024-09-19 11:15:29 +0600

    account balance update

--- js/home.js
+++ js/home.js
@@ -6,9 +6,26 @@ document.getElementById('button-add-money').addEventListener('click', function(e
     event.preventDefault();
     // step-2: get money to be added to the account 
     const addMoneyInput = document.getElementById('input-add-money').value;
-    console.log(addMoneyInput);
 
     // step-3: get the pin number provided
     const pinNumberInput = document.getElementById('input-pin-number').value;
-    console.log(pinNumberInput);
+
+    // step-4: verify pin number 
+    // wrong way to validate pin number
+    if(pinNumberInput === '1234'){
+
+        // step-4: get the current money
+        const balance = document.getElementById('account-balance').innerText;
+
+        // step-5: add addMoneyInput with  balance
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addMoneyNumber + balanceNumber;
+
+        // step-6: update the balance in the UI/DOM
+        document.getElementById('account-balance').innerText = newBalance;
+    }
+    else{
+        alert('Failed to add money! Please try again')
+    }
 })
